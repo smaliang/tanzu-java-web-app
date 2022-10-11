a89003acaf7c92259708ac01244737874dc24f98
smaliang <smaliang@gmail.com>
2022-10-11 23:50:17 +0800

    update code

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from K8s-summit + Spring Boot + Tanzu!";
+		return "Greetings from Spring Boot + Tanzu!";
 	}
 
 }
